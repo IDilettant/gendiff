13386f877fc68ec54dee296374b6ae850ef2c161
idilettant <dedenev1985@gmail.com>
2021-03-15 18:38:58 +0300

    Add a function that parse the command and its arguments

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,19 +1,26 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
+
 import argparse
 
 
-def generate_diff():
-    diff = argparse.ArgumentParser()
+def create_parser():
+    """Create a parser instance able to parse args of script.
+
+    return:
+        Returns the parser instance
+    """
+    parser = argparse.ArgumentParser()
     parser.add_argument('first_file', help='')
-    diff.add_argument('second_file', help='')
-    diff.add_argument('-f', '--format', help='set format of output')
-    return diff
+    parser.add_argument('second_file', help='')
+    parser.add_argument('-f', '--format', help='set format of output')
+    return parser
 
 
 def main():
-    diff = generate_diff()
-    namespace = diff.parse_args()
-    print(namespace)
+    """Run module script."""
+    parser = create_parser()
+    args = parser.parse_args()
+    print(args.first_file, args.second_file)
 
 
 if __name__ == '__main__':
