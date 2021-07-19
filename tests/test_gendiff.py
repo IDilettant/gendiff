ff9af939d9ddd7015c86ef06ade0957a4c97d840
idilettant <dedenev1985@gmail.com>
2021-07-19 16:40:26 +0300

    Create test for yaml extension

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -2,5 +2,9 @@
 from gendiff.scripts.gendiff import generate_diff
 
 
-def test_generate_diff(first_file, second_file, diff_result):
-    assert generate_diff(first_file, second_file) == diff_result
+def test_generate_diff_json(first_file_json, second_file_json, diff_result):
+    assert generate_diff(first_file_json, second_file_json) == diff_result
+
+
+def test_generate_diff_yaml(first_file_yml, second_file_yml, diff_result):
+    assert generate_diff(first_file_yml, second_file_yml) == diff_result
