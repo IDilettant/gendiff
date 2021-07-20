c6cc081d2d567391b62ae743135ee8936cf3cdc0
idilettant <dedenev1985@gmail.com>
2021-07-20 17:36:47 +0300

    Add fixtures for nested structure files

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,8 +7,8 @@ import yaml
 
 
 @pytest.fixture
-def first_file_content() -> Dict:
-    """File content.
+def first_file_content_flat() -> Dict:
+    """File content with flat structure.
 
     Returns:
         file content
@@ -22,8 +22,8 @@ def first_file_content() -> Dict:
 
 
 @pytest.fixture
-def second_file_content() -> Dict:
-    """File content.
+def second_file_content_flat() -> Dict:
+    """File content with flat structure.
 
     Returns:
         file content
@@ -36,80 +36,80 @@ def second_file_content() -> Dict:
 
 
 @pytest.fixture
-def first_file_json(tmpdir, first_file_content: Dict):  # noqa: WPS442
-    """First example json-file.
+def first_file_flat_json(tmpdir, first_file_content_flat: Dict):  # noqa: WPS442
+    """First example json-file with flat structure.
 
     Args:
         tmpdir: temporary directory fixture
-        first_file_content: file content
+        first_file_content_flat: file content
 
     Yields:
         temporary file
     """
     temp_file = tmpdir.join('file1.json')
-    with temp_file.open(mode='w') as temp:
-        json.dump(first_file_content, temp)
+    with temp_file.open(mode='w') as temp:  # noqa WPS204
+        json.dump(first_file_content_flat, temp)
     yield temp_file
     temp_file.remove()
 
 
 @pytest.fixture
-def second_file_json(tmpdir, second_file_content: Dict):  # noqa: WPS442
-    """Second example json-file.
+def second_file_flat_json(tmpdir, second_file_content_flat: Dict):  # noqa: WPS442 E501
+    """Second example json-file with flat structure.
 
     Args:
         tmpdir: temporary directory fixture
-        second_file_content: file content
+        second_file_content_flat: file content
 
     Yields:
         temporary file
     """
     temp_file = tmpdir.join('file2.json')
     with temp_file.open(mode='w') as temp:
-        json.dump(second_file_content, temp)
+        json.dump(second_file_content_flat, temp)
     yield temp_file
     temp_file.remove()
 
 
 @pytest.fixture
-def first_file_yml(tmpdir, first_file_content: Dict):  # noqa: WPS442
-    """First example yaml-file.
+def first_file_flat_yml(tmpdir, first_file_content_flat: Dict):  # noqa: WPS442
+    """First example yaml-file with flat structure.
 
     Args:
         tmpdir: temporary directory fixture
-        first_file_content: file content
+        first_file_content_flat: file content
 
     Yields:
         temporary file
     """
     temp_file = tmpdir.join('file1.yaml')
     with temp_file.open(mode='w') as temp:
-        yaml.dump(first_file_content, temp)
+        yaml.dump(first_file_content_flat, temp)
     yield temp_file
     temp_file.remove()
 
 
 @pytest.fixture
-def second_file_yml(tmpdir, second_file_content: Dict):  # noqa: WPS442
-    """Second example yaml-file.
+def second_file_flat_yml(tmpdir, second_file_content_flat: Dict):  # noqa: WPS442 E501
+    """Second example yaml-file with flat structure.
 
     Args:
         tmpdir: temporary directory fixture
-        second_file_content: file content
+        second_file_content_flat: file content
 
     Yields:
         temporary file
     """
     temp_file = tmpdir.join('file2.yml')  # check with alternate extension name
     with temp_file.open(mode='w') as temp:
-        yaml.dump(second_file_content, temp)
+        yaml.dump(second_file_content_flat, temp)
     yield temp_file
     temp_file.remove()
 
 
 @pytest.fixture
-def diff_result() -> str:
-    """Two json-files diff result.
+def flat_diff_result() -> str:
+    """Two json-files with flat structure diff result.
 
     Returns:
         two json-files diff result
@@ -121,4 +121,206 @@ def diff_result() -> str:
   - timeout: 50
   + timeout: 20
   + verbose: True
-}"""
+}
+"""  # noqa: WPS462
+
+
+@pytest.fixture
+def first_file_content_nested() -> Dict:
+    """File content with nested structure.
+
+    Returns:
+        file content
+    """
+    return {
+        'common': {
+            'setting1': 'Value 1',
+            'setting2': 200,
+            'setting3': True,
+            'setting6': {
+                'key': 'value',
+                'doge': {
+                    'wow': '',
+                },
+            },
+        },
+        'group1': {
+            'baz': 'bas',
+            'foo': 'bar',
+            'nest': {
+                'key': 'value',
+            },
+        },
+        'group2': {
+            'abc': 12345,
+            'deep': {
+                'id': 45,
+            },
+        },
+    }
+
+
+@pytest.fixture
+def second_file_content_nested() -> Dict:
+    """File content with nested structure.
+
+    Returns:
+        file content
+    """
+    return {
+        'common': {
+            'follow': False,
+            'setting1': 'Value 1',
+            'setting3': None,
+            'setting4': 'blah blah',
+            'setting5': {
+                'key5': 'value5',
+            },
+            'setting6': {
+                'key': 'value',
+                'ops': 'vops',
+                'doge': {
+                    'wow': 'so much',
+                },
+            },
+        },
+        'group1': {
+            'foo': 'bar',
+            'baz': 'bars',
+            'nest': 'str',
+        },
+        'group3': {
+            'deep': {
+                'id': {
+                    'number': 45,
+                },
+            },
+            'fee': 100500,
+        },
+    }
+
+
+@pytest.fixture
+def first_file_nested_yaml(tmpdir, first_file_content_nested: Dict):  # noqa: WPS442 E501
+    """First example yaml-file with flat structure.
+
+    Args:
+        tmpdir: temporary directory fixture
+        first_file_content_nested: file content
+
+    Yields:
+        temporary file
+    """
+    temp_file = tmpdir.join('file1.yaml')
+    with temp_file.open(mode='w') as temp:
+        yaml.dump(first_file_content_flat, temp)
+    yield temp_file
+    temp_file.remove()
+
+
+@pytest.fixture
+def second_file_nested_yaml(tmpdir, second_file_content_nested: Dict):  # noqa: WPS442 E501
+    """First example yaml-file with flat structure.
+
+    Args:
+        tmpdir: temporary directory fixture
+        second_file_content_nested: file content
+
+    Yields:
+        temporary file
+    """
+    temp_file = tmpdir.join('file2.yaml')
+    with temp_file.open(mode='w') as temp:
+        yaml.dump(first_file_content_flat, temp)
+    yield temp_file
+    temp_file.remove()
+
+
+@pytest.fixture
+def first_file_nested_json(tmpdir, first_file_content_nested: Dict):  # noqa: WPS442 E501
+    """First example yaml-file with flat structure.
+
+    Args:
+        tmpdir: temporary directory fixture
+        first_file_content_nested: file content
+
+    Yields:
+        temporary file
+    """
+    temp_file = tmpdir.join('file1.yaml')
+    with temp_file.open(mode='w') as temp:
+        json.dump(first_file_content_flat, temp)
+    yield temp_file
+    temp_file.remove()
+
+
+@pytest.fixture
+def second_file_nested_json(tmpdir, second_file_content_nested: Dict):  # noqa: WPS442 E501
+    """First example yaml-file with flat structure.
+
+    Args:
+        tmpdir: temporary directory fixture
+        second_file_content_nested: file content
+
+    Yields:
+        temporary file
+    """
+    temp_file = tmpdir.join('file2.yaml')
+    with temp_file.open(mode='w') as temp:
+        json.dump(first_file_content_flat, temp)
+    yield temp_file
+    temp_file.remove()
+
+
+@pytest.fixture
+def nested_diff_result() -> str:
+    """Two json-files with flat structure diff result.
+
+    Returns:
+        two json-files diff result
+    """
+    return """{
+    common: {
+      + follow: False
+        setting1: Value 1
+      - setting2: 200
+      - setting3: True
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow:
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
+}
+"""  # noqa: WPS462 W291
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -2,9 +2,9 @@
 from gendiff.scripts.gendiff import generate_diff
 
 
-def test_generate_diff_json(first_file_json, second_file_json, diff_result):
-    assert generate_diff(first_file_json, second_file_json) == diff_result
+def test_generate_diff_json(first_file_json, second_file_json, flat_diff_result):
+    assert generate_diff(first_file_json, second_file_json) == flat_diff_result
 
 
-def test_generate_diff_yaml(first_file_yml, second_file_yml, diff_result):
-    assert generate_diff(first_file_yml, second_file_yml) == diff_result
+def test_generate_diff_yaml(first_file_yml, second_file_yml, flat_diff_result):
+    assert generate_diff(first_file_yml, second_file_yml) == flat_diff_result
