2ac83eafa32fc4b6bd8f2db947dfa3e63f507c87
idilettant <dedenev1985@gmail.com>
2021-07-20 20:53:29 +0300

    Create tests for nested structure files

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -2,9 +2,45 @@
 from gendiff.scripts.gendiff import generate_diff
 
 
-def test_generate_diff_json(first_file_json, second_file_json, flat_diff_result):
-    assert generate_diff(first_file_json, second_file_json) == flat_diff_result
+def test_generate_diff_json_flat(
+    first_file_flat_json,
+    second_file_flat_json,
+    flat_diff_result,
+):
+    assert generate_diff(
+        first_file_flat_json,
+        second_file_flat_json,
+    ) == flat_diff_result
 
 
-def test_generate_diff_yaml(first_file_yml, second_file_yml, flat_diff_result):
-    assert generate_diff(first_file_yml, second_file_yml) == flat_diff_result
+def test_generate_diff_yaml_flat(
+    first_file_flat_yml,
+    second_file_flat_yml,
+    flat_diff_result,
+):
+    assert generate_diff(
+        first_file_flat_yml,
+        second_file_flat_yml,
+    ) == flat_diff_result
+
+
+def test_generate_diff_json_nested(
+    first_file_nested_json,
+    second_file_nested_json,
+    nested_diff_result,
+):
+    assert generate_diff(
+        first_file_nested_json,
+        second_file_nested_json,
+    ) == nested_diff_result
+
+
+def test_generate_diff_yaml_nested(
+    first_file_nested_yaml,
+    second_file_nested_yaml,
+    nested_diff_result,
+):
+    assert generate_diff(
+        first_file_nested_yaml,
+        second_file_nested_yaml,
+    ) == nested_diff_result
