43061e3b2a256952db0607e84eaa6315cf32bd29
idilettant <dedenev1985@gmail.com>
2021-07-25 14:36:12 +0300

    Add opportunity to use package as a library

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,4 +1,16 @@
 """Init package module."""
+from gendiff.scripts.gendiff import (
+    convert_file_content,
+    generate_diff,
+    get_diffs_repr,
+)
 from gendiff.scripts.shell_parser import create_parser
+from gendiff.scripts.stylish import stylish
 
-__all__ = ('create_parser',)  # noqa: WPS410
+__all__ = (  # noqa: WPS410
+    'create_parser',
+    'stylish',
+    'generate_diff',
+    'convert_file_content',
+    'get_diffs_repr',
+)
