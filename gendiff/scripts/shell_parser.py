9bb1cca27bfadf0b9af285dd1c6205ee785bf85c
idilettant <dedenev1985@gmail.com>
2021-07-25 14:27:34 +0300

    Moved the parser into a separate module

--- gendiff/scripts/shell_parser.py
+++ gendiff/scripts/shell_parser.py
@@ -1,6 +1,8 @@
 """Shell parser module."""
 from argparse import ArgumentParser
 
+from gendiff.scripts.stylish import stylish
+
 
 def create_parser() -> ArgumentParser:
     """Create a parser instance able to parse args of script.
@@ -11,5 +13,10 @@ def create_parser() -> ArgumentParser:
     parser = ArgumentParser()
     parser.add_argument('first_file', help='')
     parser.add_argument('second_file', help='')
-    parser.add_argument('-f', '--format', help='set format of output')
+    parser.add_argument(
+        '-f',
+        '--format',
+        default=stylish,
+        help='set format of output',
+    )
     return parser
