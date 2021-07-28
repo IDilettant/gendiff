1b352b8499c6390189e4ae8841ce4a43d0dab221
idilettant <dedenev1985@gmail.com>
2021-07-28 17:22:45 +0300

    Add functions to gendiff library usage

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,16 +1,17 @@
 """Init package module."""
-from gendiff.scripts.gendiff import (
-    convert_file_content,
-    generate_diff,
-    get_diffs_repr,
-)
-from gendiff.scripts.shell_parser import create_parser
-from gendiff.scripts.stylish import stylish
+from gendiff.differ import get_diffs_tree, parse_file_content
+from gendiff.formatters.json_formatter import format_to_json
+from gendiff.formatters.plain import plain
+from gendiff.formatters.stylish import stylish
+from gendiff.scripts.gendiff import generate_diff
+from gendiff.shell_parser import create_parser
 
 __all__ = (  # noqa: WPS410
     'create_parser',
     'stylish',
+    'plain',
+    'format_to_json',
     'generate_diff',
-    'convert_file_content',
-    'get_diffs_repr',
+    'parse_file_content',
+    'get_diffs_tree',
 )
