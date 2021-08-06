c1939125b043ef3baaaefa7b2e07e967520119d7
idilettant <dedenev1985@gmail.com>
2021-08-06 13:16:49 +0300

    Move content parser to separate module

--- gendiff/file_reader.py
+++ gendiff/file_reader.py
@@ -1,32 +1,22 @@
 """Content parser module."""
-import json
 from os import path
 from typing import Dict
 
-import yaml
+from gendiff.parser import parse_file_content
 
 
-def read_file(file_path: str):
+def read_file(file_path: str) -> Dict:
     """Read JSON or YAML files.
 
     Args:
         file_path: path to file
 
     Returns:
-        dictionary
+        converted dictionary-like file content
     """
     file_content = _extract_file_content(file_path)
     extension = _get_extension(file_path)
-    return _parse_file_content(file_content, extension)
-
-
-def _parse_file_content(file_content: str, extension: str) -> Dict:
-    json_ext = '.json'
-    yaml_ext = ('.yaml', '.yml')
-    if extension == json_ext:
-        return json.loads(file_content)
-    elif extension in yaml_ext:
-        return yaml.safe_load(file_content)
+    return parse_file_content(file_content, extension)
 
 
 def _extract_file_content(file_path: str) -> str:
---
+++ gendiff/parser.py
@@ -0,0 +1,23 @@
+"""Parser module."""
+import json
+from typing import Dict
+
+import yaml
+
+
+def parse_file_content(file_content: str, extension: str) -> Dict:
+    """Parse content from JSON or YAML file.
+
+    Args:
+        file_content: content from file
+        extension: extension of file
+
+    Returns:
+        converted content
+    """
+    json_ext = '.json'
+    yaml_ext = ('.yaml', '.yml')
+    if extension == json_ext:
+        return json.loads(file_content)
+    elif extension in yaml_ext:
+        return yaml.safe_load(file_content)
