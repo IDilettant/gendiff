fa913ad39c1e6425f123ce6ce53c744661d9ba8b
idilettant <dedenev1985@gmail.com>
2021-08-20 18:21:31 +0300

    Add doc and type hints

--- gendiff/diff_sorter.py
+++ gendiff/diff_sorter.py
@@ -1,8 +1,17 @@
 """Sorters for diff tree file."""
 from collections import OrderedDict
+from typing import Dict
 
 
-def sort_with_abc_order(diffs_tree):
+def sort_with_abc_order(diffs_tree: Dict) -> OrderedDict:
+    """Sort diffs tree with according alphabetical order.
+
+    Args:
+        diffs_tree: registered result of compare between two files
+
+    Returns:
+        sorted registered result of compare between two files
+    """
     sorted_diffs_tree = OrderedDict()
     for key, value in sorted(diffs_tree.items()):
         sorted_diffs_tree[key] = value
