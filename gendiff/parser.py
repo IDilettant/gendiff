df9f8ce54988eda9469bfb5decc76fe93afd1376
idilettant <dedenev1985@gmail.com>
2021-08-20 13:41:34 +0300

    Add case insensitive in extension extracting

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -20,6 +20,7 @@ def parse_file_content(  # type: ignore
     """
     json_ext = '.json'
     yaml_ext = ('.yaml', '.yml')
+    extension = extension.lower()
     if extension == json_ext:
         return json.loads(file_content)
     elif extension in yaml_ext:
