c838265260a61b624fee52d240ea561650371d94
idilettant <dedenev1985@gmail.com>
2021-09-01 13:39:44 +0300

    Refactor tests into parameterized style

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,104 +1,69 @@
 """Tests module."""
 import json
 
-from gendiff.cli import create_parser
-from gendiff.scripts.gendiff import generate_diff
+import pytest
+from gendiff.differ import generate_diff
+from gendiff.formatters.formats import JSON, PLAIN, STYLISH
 
 
-def test_json_flat(
-    first_file_flat_json: str,
-    second_file_flat_json: str,
-    flat_diff_result: str,
-):
-    assert generate_diff(
-        first_file_flat_json,
-        second_file_flat_json,
-    ) == flat_diff_result
+@pytest.mark.parametrize(
+    'first_filepath, second_filepath, diff_result, formatter',
+    [
+        (
+            'fixtures/first_file_flat.json',
+            'fixtures/second_file_flat.json',
+            'fixtures/flat_diff_result.txt',
+            STYLISH,
+        ),
+        (
+            'fixtures/first_file_flat.yml',
+            'fixtures/second_file_flat.yaml',
+            'fixtures/flat_diff_result.txt',
+            STYLISH,
+        ),
+        (
+            'fixtures/first_file_nested.json',
+            'fixtures/second_file_nested.json',
+            'fixtures/nested_diff_result.txt',
+            STYLISH,
+        ),
+        (
+            'fixtures/first_file_nested.yml',
+            'fixtures/second_file_nested.yml',
+            'fixtures/nested_diff_result.txt',
+            STYLISH,
+        ),
+        (
+            'fixtures/first_file_nested.json',
+            'fixtures/second_file_nested.yml',
+            'fixtures/nested_diff_result.txt',
+            STYLISH,
+        ),
+        (
+            'fixtures/first_file_nested.json',
+            'fixtures/second_file_nested.json',
+            'fixtures/plain_diff_result.txt',
+            PLAIN,
+        ),
+    ],
+)
+def test_gendiff(first_filepath, second_filepath, diff_result, formatter):
+    with open(diff_result) as diff:
+        assert generate_diff(
+            first_filepath,
+            second_filepath,
+            formatter,
+        ) == diff.read()
 
 
-def test_yaml_flat(
-    first_file_flat_yml: str,
-    second_file_flat_yml: str,
-    flat_diff_result: str,
-):
-    assert generate_diff(
-        first_file_flat_yml,
-        second_file_flat_yml,
-    ) == flat_diff_result
-
-
-def test_json_nested(
-    first_file_nested_json: str,
-    second_file_nested_json: str,
-    nested_diff_result: str,
-):
-    assert generate_diff(
-        first_file_nested_json,
-        second_file_nested_json,
-    ) == nested_diff_result
-
-
-def test_yaml_nested(
-    first_file_nested_yaml: str,
-    second_file_nested_yaml: str,
-    nested_diff_result: str,
-):
-    assert generate_diff(
-        first_file_nested_yaml,
-        second_file_nested_yaml,
-    ) == nested_diff_result
-
-
-def test_json_yaml_nested(
-    first_file_nested_json: str,
-    second_file_nested_yaml: str,
-    nested_diff_result: str,
-):
-    assert generate_diff(
-        first_file_nested_json,
-        second_file_nested_yaml,
-    ) == nested_diff_result
-
-
-def test_create_parser(
-    first_file_flat_json: str,
-    second_file_flat_json: str,
-    flat_diff_result: str,
-):
-    parser = create_parser()
-    args = parser.parse_args([
-        str(first_file_flat_json),
-        str(second_file_flat_json),
-    ])
-    assert generate_diff(
-        args.first_file,
-        args.second_file,
-        formatter=args.format,
-    ) == flat_diff_result
-
-
-def test_plain_repr(
-    first_file_nested_json: str,
-    second_file_nested_yaml: str,
-    plain_diff_result: str,
-):
-    assert generate_diff(
-        first_file_nested_json,
-        second_file_nested_yaml,
-        formatter='plain',
-    ) == plain_diff_result
-
-
-def test_format_to_json(
-    first_file_nested_json: str,
-    second_file_nested_yaml: str,
-):
+def test_format_to_json():
     try:
-        json.loads(generate_diff(
-            first_file_nested_json,
-            second_file_nested_yaml,
-            formatter='json',
-        ))
-        assert True
+        assert json.loads(
+            generate_diff(
+                'fixtures/first_file_nested.json',
+                'fixtures/second_file_nested.yml',
+                formatter=JSON,
+            ),
+        )
     except json.JSONDecodeError:
-        assert False
+        raise AssertionError('File is not valid JSON file')
